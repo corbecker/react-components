bf5ca02be3dc66286a4295ea398e5ad0d5a4fd25
corbecker <corbinbecker@protonmail.com>
2019-07-08 15:42:31 -0700

    avatar image generation added to props

--- src/CommentDetail.js
+++ src/CommentDetail.js
@@ -1,11 +1,10 @@
 import React from "react";
-import Faker from "faker";
 
 const CommentDetail = props => {
   return (
     <div className="comment">
       <a href="/" className="avatar">
-        <img alt="avatar" src={Faker.image.avatar()} />
+        <img alt="avatar" src={props.avatar} />
       </a>
       <div className="content">
         <a href="/" className="author">
--- src/index.js
+++ src/index.js
@@ -1,20 +1,28 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import CommentDetail from "./CommentDetail";
+import faker from "faker";
 
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetail author="Mike" time="Today 6pm" text="This is a comment." />
+      <CommentDetail
+        author="Mike"
+        time="Today 6pm"
+        text="This is a comment component."
+        avatar={faker.image.avatar()}
+      />
       <CommentDetail
         author="Michelle"
         time="Today 2am"
         text="This is another comment."
+        avatar={faker.image.avatar()}
       />
       <CommentDetail
         author="Donna"
         time="Today 1pm"
         text="This is yet another comment."
+        avatar={faker.image.avatar()}
       />
     </div>
   );
